87b325ba67d392df40d942545f45c3518fbb7db0
fernandoMedina-S <jfernando.medinas@alumnos.udg.mx>
2021-04-11 16:01:05 -0500

    Procfile

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect("http://localhost:4000/");
+var socket = io.connect("https://chatfer.herokuapp.com/");
 
 var message = document.getElementById("message");
 var user = document.getElementById("user");
